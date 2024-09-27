4bd342eb57ce3947ab45afca7e5eb84cee63d00d
Travisthefish <148793709+smallbigie@users.noreply.github.com>
2024-09-27 14:53:19 +0000

    Friday, Sept. 27 2024 Progress

---
+++ basicMuliTable.py
@@ -0,0 +1,21 @@
+'''
+Program: Basic Multiplication Table
+Author: Cameron Andrus
+
+1. Ask the user for a number
+2. Print out the name of the multiplication table:
+    "Multiplication Table for <number>:"
+3. for Loop print out all the multiplication facts (1 to 12) for the users number
+'''
+
+
+
+'''
+Multiplication Table Generator with Custom Range, and Multi-Table Option
+
+1. Ask the user of they want to generate multiple tables
+2. Create a Function to do steps 2 & 3 above (3 variables: number, start=1, end=12)
+3. Create a control structure to handle the user answer from step 1
+    What do we do if yes
+    What do we do if no
+'''
